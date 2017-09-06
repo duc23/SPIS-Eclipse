f7aafb47c238b7ff5768203fd593938c168f29f4
Stephen Deslate <sdeslate@ucsd.edu>
2017-09-06 16:27:55 -0700

    run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, url_for, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return render_template('home.html')
+
+if __name__ == "__main__":
+	app.run(port=5000,debug=False)
